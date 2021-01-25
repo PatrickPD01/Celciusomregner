ddbbf03c1d1080518dbae5c365284f10f35f928f
PatrickPD01 <61233451+PatrickPD01@users.noreply.github.com>
2021-01-25 13:12:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Celciusomregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                double fahrenheit;
+
+                double celcius = 20;
+                Console.WriteLine("Celcius: " + celcius);
+
+                fahrenheit = (celcius = 20) * 1.8 + 32;
+                Console.WriteLine("Fahrenheit: " + fahrenheit);
+            }
+        }
+    }
+}
